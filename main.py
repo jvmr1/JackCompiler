2844f3e638b2f1e41c6695230c7d55522816a282
João Vitor Roma <jvmr2.0@gmail.com>
2019-04-17 21:09:49 -0300

    Minor alterations

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from CompilationEngine import CompilationEngine
 
-compEng = CompilationEngine('Square.jack')
+compEng = CompilationEngine('Main.jack')
 xml_string = compEng.compileClass()
 print(xml_string)
-with open('Square.xml', 'w') as xml:
+with open('Main.xml', 'w') as xml:
     if type(xml_string) == str:
         xml.write(xml_string)
